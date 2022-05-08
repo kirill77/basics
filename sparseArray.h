b7c3b2aab3ee788392f923ab1214245d69a90099
kirill77 <kirill77>
2022-05-08 09:58:08 -0700

    refactored SparseHierarchy

--- sparseArray.h
+++ sparseArray.h
@@ -10,79 +10,82 @@ struct SparseHierarchy
 {
     void init(NvU32 nElements, NvU32 uGroundLayer) // uGroundLayer is the layer where all elements are initially
     {
-        nvAssert(nElements > 0); // don't need this corner case at the moment
-        if (m_nOrigElements != nElements) // size has changed? reinitialize
+        nvAssert(nElements >= m_nOrigElements); // I only implemented adding new elements - can't shrink the array
+        if (nElements <= m_nOrigElements)
+            return;
+#if ASSERT_ONLY_CODE
+        for (NvU32 uLayer = 0; uLayer < m_pLayers.size(); ++uLayer)
         {
-            m_nOrigElements = nElements;
-            // resize to 0 and then back to full size for constructor to be called on each element
-            m_pElements.resize(0);
-            m_pElements.resize(nElements + m_pLayers.size() * 2);
-
-            for (NvU32 u = uGroundLayer; u <= uGroundLayer; --u)
-            {
-                m_pLayers[u] = Layer(); // re-initialize the layers completely
-                createLayer(u);
-            }
-
-            // link all elements into list
-            for (NvU32 u = 1; u < m_nOrigElements; ++u)
-            {
-                m_pElements[u - 1].m_uNext = u;
-                m_pElements[u].m_uPrev = u - 1;
-                m_pElements[u].m_maxLayerId = m_pLayers[uGroundLayer].m_id;
-            }
-
-            // put all elements onto ground layer
-            m_pElements[_firstLayerEl(uGroundLayer)].m_uNext = 0;
-            m_pElements[0].m_uPrev = _firstLayerEl(uGroundLayer);
-            m_pElements[_lastLayerEl(uGroundLayer)].m_uPrev = m_nOrigElements - 1;
-            m_pElements[m_nOrigElements - 1].m_uNext = _lastLayerEl(uGroundLayer);
+            // below I assume that all layers except the ground one are invalid
+            nvAssert(uLayer == uGroundLayer || !m_pLayers[uLayer].m_isValid);
         }
+#endif
+        NvU32 uGrSentinel0 = INVALID_UINT32, uGrSentinel1 = INVALID_UINT32;
+        if (m_pLayers[uGroundLayer].m_isValid && hasElements(uGroundLayer))
+        {
+            uGrSentinel0 = _firstLayerEl(uGroundLayer);
+            uGrSentinel1 = _lastLayerEl(uGroundLayer);
+        }
+        NvU32 uFirstNewElement = m_nOrigElements;
+        m_nOrigElements = nElements;
+        m_pElements.resize(nElements + m_pLayers.size() * 2);
+        if (!m_pLayers[uGroundLayer].m_isValid)
+        {
+            createLayer(uGroundLayer);
+        }
+        else
+        {
+            // copy sentinels to new place
+            m_pElements[_lastLayerEl(uGroundLayer)] = m_pElements[uGrSentinel1];
+            m_pElements[_firstLayerEl(uGroundLayer)] = m_pElements[uGrSentinel0];
+            // fix links from the existing elements to the new sentinel positions
+            linkTwoElements(_firstLayerEl(uGroundLayer), m_pElements[_firstLayerEl(uGroundLayer)].m_uNext);
+            linkTwoElements(m_pElements[_lastLayerEl(uGroundLayer)].m_uPrev, _lastLayerEl(uGroundLayer));
+        }
+
+        // link all new elements together
+        m_pElements[uFirstNewElement].m_maxLayerId = m_pLayers[uGroundLayer].m_id;
+        for (NvU32 u = uFirstNewElement + 1; u < m_nOrigElements; ++u)
+        {
+            linkTwoElements(u - 1, u);
+            m_pElements[u].m_maxLayerId = m_pLayers[uGroundLayer].m_id;
+        }
+
+        // add list of new elements to the ground layer list
+        linkTwoElements(m_pElements[_lastLayerEl(uGroundLayer)].m_uPrev, uFirstNewElement);
+        linkTwoElements(m_nOrigElements - 1, _lastLayerEl(uGroundLayer));
     }
     bool hasElements(NvU32 uLayer) const
     {
-        return m_pElements[_firstLayerEl(uLayer)].m_uNext < m_nOrigElements;
+        NvU32 uSentinel0 = _firstLayerEl(uLayer);
+        return m_pElements[uSentinel0].m_uNext != uSentinel0 + 1;
     }
-    bool hasElement(NvU32 uLayer, NvU32 uElement) const
+    bool hasEverBeenAtLayer(NvU32 uLayer, NvU32 uElement) const
     {
         nvAssert(m_pLayers[uLayer].m_isValid);
-        return m_pElements[uElement].m_maxLayerId >= m_pLayers[uLayer].m_id;
+        return m_pElements[uElement].m_maxLayerId - m_pLayers[uLayer].m_id < 0x80000000;
     }
     void moveToLayer(NvU32 uLayer, NvU32 uElement)
     {
         nvAssert(m_pLayers[uLayer].m_isValid);
-        auto& el = m_pElements[uElement];
+        Element &el = m_pElements[uElement];
         el.m_maxLayerId = m_pLayers[uLayer].m_id;
         // unlinking from whatever layer the element is currently on
-        m_pElements[el.m_uPrev].m_uNext = el.m_uNext;
-        m_pElements[el.m_uNext].m_uPrev = el.m_uPrev;
+        linkTwoElements(el.m_uPrev, el.m_uNext);
         // linking to uLayer
-        NvU32 uEl0 = _firstLayerEl(uLayer);
-        auto& el0 = m_pElements[uEl0];
-        NvU32 uEl2 = el0.m_uNext;
-        el0.m_uNext = uElement;
-        el.m_uPrev = uEl0;
-        el.m_uNext = uEl2;
-        m_pElements[uEl2].m_uPrev = uElement;
+        linkTwoElements(uElement, m_pElements[_firstLayerEl(uLayer)].m_uNext);
+        linkTwoElements(_firstLayerEl(uLayer), uElement);
     }
     void createLayer(NvU32 uLayer)
     {
         nvAssert(!m_pLayers[uLayer].m_isValid);
         m_pLayers[uLayer].m_isValid = true;
         m_pLayers[uLayer].m_id = ++m_lastLayerId;
-        NvU32 uEl0 = _firstLayerEl(uLayer);
-        NvU32 uEl1 = _lastLayerEl(uLayer);
-        m_pElements[uEl0].m_uNext = uEl1;
-        m_pElements[uEl1].m_uPrev = uEl0;
+        linkTwoElements(_firstLayerEl(uLayer), _lastLayerEl(uLayer));
     }
     void destroyLayer(NvU32 uLayer)
     {
-        nvAssert(m_pLayers[uLayer].m_isValid);
-        if (hasElements(uLayer))
-        {
-            // since this layer is destroyed - all its elements are moved to the previous layer
-            moveAllElements(uLayer - 1, uLayer);
-        }
+        nvAssert(m_pLayers[uLayer].m_isValid && !hasElements(uLayer));
         m_pLayers[uLayer].m_isValid = false;
     }
     void moveAllElements(NvU32 uDstLayer, NvU32 uSrcLayer)
@@ -90,22 +93,14 @@ struct SparseHierarchy
         nvAssert(m_pLayers[uDstLayer].m_isValid && m_pLayers[uSrcLayer].m_isValid);
         if (!hasElements(uSrcLayer))
             return;
-        // this condition is required for hasElement() to work correctly
-        nvAssert(m_pLayers[uDstLayer].m_id < m_pLayers[uSrcLayer].m_id);
-        NvU32 uSrcFirst = _firstLayerEl(uSrcLayer);
-        NvU32 uSrcLast = _lastLayerEl(uSrcLayer);
-
-        NvU32 uEl0 = _firstLayerEl(uDstLayer);
-        NvU32 uEl1 = m_pElements[uSrcFirst].m_uNext;
-        NvU32 uEl2 = m_pElements[uSrcLast].m_uPrev;
-        NvU32 uEl3 = m_pElements[uEl0].m_uNext;
-        m_pElements[uEl0].m_uNext = uEl1;
-        m_pElements[uEl1].m_uPrev = uEl0;
-        m_pElements[uEl2].m_uNext = uEl3;
-        m_pElements[uEl3].m_uPrev = uEl2;
-
-        m_pElements[uSrcFirst].m_uNext = uSrcLast;
-        m_pElements[uSrcLast].m_uPrev = uSrcFirst;
+        // this condition is required for hasEverBeenAtLayer() to work correctly
+        nvAssert(m_pLayers[uSrcLayer].m_id - m_pLayers[uDstLayer].m_id < 0x80000000);
+        // link the end of src chain to the beginning of dest chain
+        linkTwoElements(m_pElements[_lastLayerEl(uSrcLayer)].m_uPrev, m_pElements[_firstLayerEl(uDstLayer)].m_uNext);
+        // link first dst sentinel with the beginning of src chain
+        linkTwoElements(_firstLayerEl(uDstLayer), m_pElements[_firstLayerEl(uSrcLayer)].m_uNext);
+        // link two src sentinels together
+        linkTwoElements(_firstLayerEl(uSrcLayer), _lastLayerEl(uSrcLayer));
     }
     NvU32 getFirstLayerElement(NvU32 uLayer) const
     {
@@ -117,6 +112,11 @@ struct SparseHierarchy
         return m_pElements[uElement].m_uNext;
     }
 private:
+    void linkTwoElements(NvU32 uEl0, NvU32 uEl1)
+    {
+        m_pElements[uEl0].m_uNext = uEl1;
+        m_pElements[uEl1].m_uPrev = uEl0;
+    }
     NvU32 _firstLayerEl(NvU32 uLayer) const
     {
         nvAssert(m_pLayers[uLayer].m_isValid);
