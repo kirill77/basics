012afc37a50cf859f19ddc6160a923cc86b0542e
Kirill Dmitriev <dkirill@nvidia.com>
2022-08-03 11:43:32 -0700

    added TextWriter and changed related stuff (added indent implementation for instance)

--- serializer.h
+++ serializer.h
@@ -4,6 +4,17 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <stack>
+#include <memory>
+
+struct Indent
+{
+    Indent(struct ISerializer& s, const char* sName) : m_serializer(s), m_sName(sName) { }
+    ~Indent();
+private:
+    ISerializer& m_serializer;
+    const char* m_sName = nullptr;
+};
 
 struct ISerializer
 {
@@ -13,35 +24,37 @@ struct ISerializer
     }
 
     template <class T>
-    void serializeArraySize(std::vector<T>& p)
+    void serializeArraySize(const char *sName, std::vector<T>& p)
     {
         size_t size = p.size();
-        serializePreallocatedMem(&size, sizeof(size));
+        serializeSimpleType("arraySize", size);
         p.resize(size);
     }
 
     template <class T>
-    void serializeStdArray(std::vector<T>& p)
+    void serializeStdArray(const char *sName, std::vector<T>& p)
     {
-        serializeArraySize(p);
+        std::shared_ptr<Indent> pIndent = pushIndent(sName);
+        serializeArraySize(sName, p);
         if (p.size() > 0)
         {
-            serializePreallocatedMem(&p[0], sizeof(p[0]) * (NvU32)p.size());
+            serializePreallocatedMem(sName, &p[0], sizeof(p[0]) * (NvU32)p.size());
         }
     }
 
     template <class T>
-    void serializeArrayOfPointers(std::vector<T*>& p)
+    void serializeArrayOfPointers(const char *sName, std::vector<T*>& p)
     {
+        std::shared_ptr<Indent> pIndent = pushIndent(sName);
         // serialize array size
-        serializeArraySize(p);
+        serializeArraySize(sName, p);
 
         // serialize each pointer in the array
         for (NvU32 u = 0; u < p.size(); ++u)
         {
-            NvU32 exists = (p[u] != nullptr);
-            serializePreallocatedMem(&exists, sizeof(exists));
-            if (!exists)
+            NvU32 ptrExists = (p[u] != nullptr);
+            serializeSimpleType("ptrExists", ptrExists);
+            if (!ptrExists)
             {
                 continue;
             }
@@ -49,16 +62,56 @@ struct ISerializer
             {
                 p[u] = new T();
             }
-            p[u]->serialize(*this);
+            char sBuffer[16];
+            sprintf_s(sBuffer, "[%d]", u);
+            std::shared_ptr<Indent> pIndent = pushIndent(sBuffer);
+            p[u]->serialize(sName, *this);
         }
     }
 
     template <class T>
-    void serializeSimpleType(T& value)
+    void serializeArrayOfSharedPtrs(const char *sName, std::vector<std::shared_ptr<T>>& refs)
     {
-        serializePreallocatedMem(&value, sizeof(value));
+        std::shared_ptr<Indent> pIndent = pushIndent(sName);
+        serializeArraySize(sName, refs);
+        
+        // serialize each shared_ptr in the array
+        for (NvU32 u = 0; u < refs.size(); ++u)
+        {
+            NvU32 refExists = (refs[u] != nullptr);
+            serializeSimpleType("refExists", refExists);
+            if (!refExists)
+            {
+                continue;
+            }
+            if (refs[u] == nullptr)
+            {
+                refs[u] = std::make_shared<T>();
+            }
+            char sBuffer[16];
+            sprintf_s(sBuffer, "[%d]", u);
+            std::shared_ptr<Indent> pIndent = pushIndent(sBuffer);
+            refs[u]->serialize(sName, *this);
+        }
     }
-    virtual void serializePreallocatedMem(void* pMem, NvU32 memSizeInBytes) = 0;
+
+
+    template <class T>
+    void serializeSimpleType(const char *sName, T & value)
+    {
+        serializePreallocatedMem(sName, &value, sizeof(value));
+    }
+    virtual void serializePreallocatedMem(const char *sName, void* pMem, NvU32 memSizeInBytes) = 0;
+
+    virtual std::shared_ptr<Indent> pushIndent(const char* sName)
+    {
+        return nullptr; // by default indents are ignored
+    }
+    void notifyIndentDestroyed(Indent *p)
+    {
+        nvAssert(p == m_pIndents.top());
+        m_pIndents.pop();
+    }
 
 protected:
     bool m_isReading = false;
@@ -68,6 +121,7 @@ protected:
             return path;
         return std::wstring(L"c:\\atomNets\\") + path.c_str();
     }
+    std::stack<Indent *> m_pIndents;
 };
 
 struct MyWriter : public ISerializer
@@ -82,7 +136,7 @@ struct MyWriter : public ISerializer
     {
         fclose(m_fp);
     }
-    virtual void serializePreallocatedMem(void* pMem, NvU32 memSizeInBytes) override
+    virtual void serializePreallocatedMem(const char *sName, void* pMem, NvU32 memSizeInBytes) override
     {
         nvAssert(m_fp != nullptr && memSizeInBytes > 0);
         fwrite(pMem, 1, memSizeInBytes, m_fp);
@@ -104,7 +158,7 @@ struct MyReader : public ISerializer
     {
         fclose(m_fp);
     }
-    virtual void serializePreallocatedMem(void* pMem, NvU32 memSizeInBytes) override
+    virtual void serializePreallocatedMem(const char *sName, void* pMem, NvU32 memSizeInBytes) override
     {
         nvAssert(m_fp != nullptr);
         fread(pMem, 1, memSizeInBytes, m_fp);
@@ -112,3 +166,49 @@ struct MyReader : public ISerializer
 private:
     FILE* m_fp = nullptr;
 };
+
+struct TextWriter : public ISerializer
+{
+    TextWriter(char* sFileName)
+    {
+        fopen_s(&m_fp, sFileName, "wt");
+    }
+    ~TextWriter()
+    {
+        nvAssert(m_fp != nullptr);
+        fclose(m_fp);
+    }
+
+    virtual void serializePreallocatedMem(const char *sName, void* pMem, NvU32 memSizeInBytes) override
+    {
+        printCurIndent();
+        nvAssert(m_fp != nullptr);
+        fprintf(m_fp, "%s:\n", sName);
+    }
+
+    virtual std::shared_ptr<Indent> pushIndent(const char* sName) override
+    {
+        printCurIndent();
+        nvAssert(m_fp != nullptr);
+        fprintf(m_fp, "%s\n", sName);
+        std::shared_ptr<Indent> p = std::make_shared<Indent>(*this, sName);
+        m_pIndents.push(p.get());
+        return p;
+    }
+
+private:
+    void printCurIndent()
+    {
+        nvAssert(m_fp != nullptr);
+        for (int i = 0; i < m_pIndents.size(); ++i)
+        {
+            fprintf(m_fp, "  ");
+        }
+    }
+    FILE* m_fp = nullptr;
+};
+
+inline Indent::~Indent()
+{
+    m_serializer.notifyIndentDestroyed(this);
+}
